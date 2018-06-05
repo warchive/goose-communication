32805644b9310d639fca8ed16684ccca099af450
Shahzad Lone <shahzadlone@gmail.com>
2018-06-05 00:16:21 -0400

    Rough "Test Like" Implementation Done (#13)
    * state machine deleted (only coms in this repo)
    
    * websockets implemented
    
    * Made a basic sending and receiving template that encodes and decodes.
    
    * Added a "make debug-server" target
    
    This target is used to quickly build the binary turning
    compiler optimizations off and keeping debugging symbols, and opening
    the binary using gdb.
    
    * Bytes conversion from packet to json working
    
    Got the bytes conversion from packet and the json
    convertion from packet working and integrating with the servers.
    
    * formmating fix, vendor packages and README updated

--- cmd/comms/comms.go
+++ cmd/comms/comms.go
@@ -3,42 +3,70 @@ package main
 import (
     "net/http"
     "github.com/gorilla/websocket"
+    "github.com/waterloop/wcomms/wjson"
+    "github.com/waterloop/wcomms/wbinary"
     "log"
     "time"
-    "strconv"
 )
 
-var upgrader = websocket.Upgrader{}
+// originalPacket converts to := []byte{178, 157, 26, 78, 167, 88, 234, 94}
+var originalPacket = &wbinary.CommPacket{
+    PacketType: wbinary.State,
+    PacketId:   54,
+    Data1:      -724.875,
+    Data2:      846.5,
+    Data3:      442.5625,
+}
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
 
 func main() {
-    timeChan := time.NewTimer(time.Minute * 60).C
-    tickChan := time.NewTicker(time.Millisecond * 1000).C
-
-    num := 0
-
-
     http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
+
         var conn, _ = upgrader.Upgrade(w, r, nil)
 
-
         go func() {
+
             for {
-                select {
-                case <-timeChan:
-                    log.Println("All packets sents")
-                case <-tickChan:
-                    conn.WriteMessage(1, []byte("Packet number "+strconv.Itoa(num)+" sent!!"))
-                    num++
-                }
+                // Write the packet after converting it to the 8-byte buffer.
+                bytesToSend := wbinary.WritePacket(originalPacket)
+
+                // Send the binary bytes of the original packet to the connection.
+                conn.WriteMessage(websocket.BinaryMessage, bytesToSend)
+                log.Printf("Sent these bytes: [%x]\n", bytesToSend)
+
+                // Sleep before sending the next packet.
+                time.Sleep(time.Second * 5)
+
             }
         }()
 
-        // read data from the client
+        // Read data from the client
         go func(conn *websocket.Conn) {
-             for  {
-                  _, msg, _ := conn.ReadMessage()
-                  log.Println("Received: " + string(msg))
-             }
+            for {
+                // Try to receive/read the bytes (that were sent).
+                receivedType, receivedBytes, _ := conn.ReadMessage()
+
+                // We only care if the message is binary (discard the rest).
+                if receivedType == websocket.BinaryMessage {
+                    log.Printf("Recieved these bytes: [%x]\n", receivedBytes)
+
+                    // Read the received bytes and make a packet out of them.
+                    receivedPacket := wbinary.ReadPacket(receivedBytes)
+
+                    // Encode that packet, to make it pretty (JSON).
+                    encodedPacket, thereWasAnError := wjson.PacketEncodeJson(receivedPacket)
+                    if thereWasAnError != nil {
+                        panic(thereWasAnError)
+                    } else {
+                        log.Println("Encoded the received packet to JSON: ")
+                        log.Printf("%s\n", encodedPacket)
+                    }
+                } // End of check to see if the message we received was indeed binary.
+            } // End of for-loop.
         }(conn)
     })
 
