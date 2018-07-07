ad31163fec138028dd01b7e4f9b627709660fca8
Deep Dhillon <deep@deepdhillon.ca>
2018-07-07 15:38:48 -0400

    made it use json data

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:3000/v1/ws');
+const ws = new WebSocket('ws://localhost:6500/v1/ws');
 
 ws.on('open', function open() {
     ws.send('Connection Established');
--- cmd/comms/comms.go
+++ cmd/comms/comms.go
@@ -3,72 +3,132 @@ package main
 import (
     "net/http"
     "github.com/gorilla/websocket"
-    "github.com/waterloop/wcomms/wjson"
     "github.com/waterloop/wcomms/wbinary"
     "log"
     "time"
+    "strconv"
+    "encoding/json"
+    "os"
 )
 
-// originalPacket converts to := []byte{178, 157, 26, 78, 167, 88, 234, 94}
-var originalPacket = &wbinary.CommPacket{
-    PacketType: wbinary.State,
-    PacketId:   54,
-    Data1:      -724.875,
-    Data2:      846.5,
-    Data3:      442.5625,
-}
+
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
 }
 
+func PrintPackate(packet *wbinary.CommPacket) {
+    log.Println("Type: " + string(packet.PacketType) + " ID: " + string(packet.PacketId) +
+        "Data: [" + strconv.FormatFloat(float64(packet.Data1), 'f', 2, 32) + "," +
+        strconv.FormatFloat(float64(packet.Data2), 'f', 2, 32) + "," +
+        strconv.FormatFloat(float64(packet.Data3), 'f', 2, 32)  + "]")
+}
+
 func main() {
+    // create initial mag wheels packet
+    var magPacket = &wbinary.CommPacket{
+        PacketType: wbinary.Sensor,
+        PacketId:   0,
+        Data1:      0,
+        Data2:      0,
+        Data3:      0,
+    }
+
+    // create initial friction packet
+    var frictionPacket = &wbinary.CommPacket{
+        PacketType: wbinary.Sensor,
+        PacketId:   1,
+        Data1:      0,
+        Data2:      0,
+        Data3:      0,
+    }
+
+    // create initial levitation packet
+    var levPacket = &wbinary.CommPacket{
+        PacketType: wbinary.Sensor,
+        PacketId:   2,
+        Data1:      0,
+        Data2:      0,
+        Data3:      0,
+    }
+
+    // which packet to send
+    packetType := 0
+    var err error
+
     http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
 
         var conn, _ = upgrader.Upgrade(w, r, nil)
 
         go func() {
-
             for {
-                // Write the packet after converting it to the 8-byte buffer.
-                bytesToSend := wbinary.WritePacket(originalPacket)
+                var bytesToSend []byte
+
+                if packetType == 0 {
+                    bytesToSend, err = json.Marshal(magPacket)
+                    if err != nil {
+                        log.Println(err.Error())
+                        os.Exit(2)
+                    }
+                    magPacket.Data1 += 1
+
+                    if magPacket.Data1 > 100 {
+                        magPacket.Data1 = 0
+                    }
+                } else if packetType == 1 {
+                    bytesToSend, err = json.Marshal(frictionPacket)
+                    if err != nil {
+                        log.Println(err.Error())
+                        os.Exit(2)
+                    }
+                    frictionPacket.Data1 += 1
+
+                    if frictionPacket.Data1 > 100 {
+                        frictionPacket.Data1 = 0
+                    }
+                } else if packetType == 2 {
+                    bytesToSend, err = json.Marshal(levPacket)
+                    if err != nil {
+                        log.Println(err.Error())
+                        os.Exit(2)
+                    }
+                    levPacket.Data1 += 1
+
+                    if levPacket.Data1 > 100 {
+                        levPacket.Data1 = 0
+                    }
+                } else {
+                    packetType = 0
+                    continue
+                }
+
+                log.Println(packetType)
 
                 // Send the binary bytes of the original packet to the connection.
-                conn.WriteMessage(websocket.BinaryMessage, bytesToSend)
-                log.Printf("Sent these bytes: [%x]\n", bytesToSend)
+                conn.WriteMessage(websocket.TextMessage, bytesToSend)
+
+                log.Printf("Sent this json %s, \n", string(bytesToSend))
 
                 // Sleep before sending the next packet.
-                time.Sleep(time.Second * 5)
-
+                time.Sleep(time.Second * 1)
+                packetType += 1
             }
         }()
 
-        // Read data from the client
+        // Read data from the client and print to the console
         go func(conn *websocket.Conn) {
             for {
                 // Try to receive/read the bytes (that were sent).
                 receivedType, receivedBytes, _ := conn.ReadMessage()
 
-                // We only care if the message is binary (discard the rest).
-                if receivedType == websocket.BinaryMessage {
-                    log.Printf("Recieved these bytes: [%x]\n", receivedBytes)
-
-                    // Read the received bytes and make a packet out of them.
-                    receivedPacket := wbinary.ReadPacket(receivedBytes)
-
-                    // Encode that packet, to make it pretty (JSON).
-                    encodedPacket, thereWasAnError := wjson.PacketEncodeJson(receivedPacket)
-                    if thereWasAnError != nil {
-                        panic(thereWasAnError)
-                    } else {
-                        log.Println("Encoded the received packet to JSON: ")
-                        log.Printf("%s\n", encodedPacket)
-                    }
-                } // End of check to see if the message we received was indeed binary.
+                // we only care if the message is string
+                if receivedType == websocket.TextMessage {
+                    log.Printf("Received json %s\n", string(receivedBytes))
+                }
             } // End of for-loop.
         }(conn)
     })
 
-    http.ListenAndServe(":3000", nil)
+    http.ListenAndServe(":6500", nil)
 }
